d7a2b7f6f474f97fae6e31bc41f449cf14f661f8
Andressa <andressagss21@gmail.com>
2023-12-01 13:06:23 -0300

    delete actions.jsx

--- src/pages/actions.jsx
+++
@@ -1,7 +0,0 @@
-export const fetchGit = async() => {
-  const response = await fetch ("https://api.github.com/users/andressagabrielle21/repos?sort=updated&direction=desc") 
-
-  const data = await response.json();
-
-  console.log(data)
-}
