f92ad48cb2ccf2c4386ed5786b324e1103ba7ead
Andressa <andressagss21@gmail.com>
2023-12-04 22:57:03 -0300

    added favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;400;600;800&family=Oleo+Script+Swash+Caps:wght@400;700&display=swap" rel="stylesheet" />
