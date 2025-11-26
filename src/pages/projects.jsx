7067fae589cf69ba3d614e9d76ec08f553fe7f48
Andressa Souza <andressagss21@gmail.com>
2025-11-26 20:09:20 -0300

    updated breakpoint on projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
 
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, x: [0, 50, 0] }} transition={{ ease: "easeIn", duration: .9 }}>
           <div className="flex items-center justify-center">
-            <div className='w-[90%] xl:w-[75%] sm:grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-8'>
+            <div className='w-[90%] 2xl:w-[75%] sm:grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-8'>
               {data.map((item) => (
                 <ProjectCard
                   title={item.repoName}
